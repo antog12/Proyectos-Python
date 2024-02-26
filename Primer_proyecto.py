e77215886df2a23c915bb3a2a1a204554920a3f9
A.Ángel Abellán <72298869+antog12@users.noreply.github.com>
2024-02-26 18:27:16 +0100

    Add files via upload

---
+++ Primer_proyecto.py
@@ -0,0 +1,26 @@
+'''print("Hola"+" "+'Anto')
+#-------------------------
+print("me llamo \"Antonio\"")
+#------------------
+print("Esto es una linea\nY esto es otra línea")
+print("\tEsta es la tercera línea")
+print("this isn\'t a number")
+print("este signo \\ es una barra invertida")
+#------------------
+print("INPUT")
+#------------------
+print("Dime tu nombre: ")
+print("Dime tu apellido: ")
+
+print(input("Dime tu nombre: "))
+input("Dime tu apellido: ")
+
+print(input("¿Qué estás estudiando?"))
+
+print(input("Escribe tu nombre: ")+" "+input("Escribe tu apellido: "))'''
+print("-----------------")
+print("Proyecto Día 1")
+print("-----------------")
+print("El nombre de tu cerveza es\n '" +input("Que ciudad te gustaría visitar?: ")+ " "+ input("Cúal es tu color favorito?: ") +"'\nFelicitaciones")
+
+
